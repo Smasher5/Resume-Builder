147c84589e23501c4eb5ec2ee2a414144fb8ecba
Smasher5 <166694726+Smasher5@users.noreply.github.com>
2026-03-08 19:30:25 +0530

    Delete Backend directory

--- Backend/config/db.js
+++
@@ -1,5 +0,0 @@
-import mongoose from 'mongoose'
-
-export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://dbMadhav:dbMadhavXRetrievo@cluster0.vq0vh3h.mongodb.net/resume-builder').then(() => console.log("DB connected"))
-}
