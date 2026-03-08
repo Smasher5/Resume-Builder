35e1ff2fe9910a3becc57ebc021c1b807ac4e0a8
Smasher5 <166694726+Smasher5@users.noreply.github.com>
2026-03-08 19:10:14 +0530

    Update apiPaths.js

--- Frontend/src/utils/apiPaths.js
+++ Frontend/src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:3000'
+export const BASE_URL = 'http://localhost:3000/'
 
 // Routes used for Frontend
 export const API_PATHS = {
@@ -20,4 +20,5 @@ export const API_PATHS = {
     image: {
         UPLOAD_IMAGE: '/api/auth/upload-image'
     }
+
 }
