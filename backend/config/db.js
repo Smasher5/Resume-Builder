be2a56f41ae3b6ae28ed212ebd00aa524a6303f8
Smasher5 <166694726+Smasher5@users.noreply.github.com>
2026-03-08 20:51:41 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose'
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://dbMadhav:dbMadhavXRetrievo@cluster0.vq0vh3h.mongodb.net/resume-builder').then(() => console.log("DB connected"))
+    await mongoose.connect('process.env.MONGO').then(() => console.log("DB connected"))
+
 }
