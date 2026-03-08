64235c2ef40aca1b0189d38af4a80d9df43b6959
Smasher5 <166694726+Smasher5@users.noreply.github.com>
2026-03-08 19:24:20 +0530

    Update apiPaths.js

--- Frontend/src/utils/apiPaths.js
+++ Frontend/src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:3000/'
+export const BASE_URL = '/'
 
 // Routes used for Frontend
 export const API_PATHS = {
@@ -22,3 +22,4 @@ export const API_PATHS = {
     }
 
 }
+
